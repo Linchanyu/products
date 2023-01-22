2f11c24f6d9cc1153c8e94d58b853cf80512103d
Linchanyu <shaddock2009@hotmail.com>
2023-01-22 15:53:09 +0800

    add write

--- products.py
+++ products.py
@@ -15,6 +15,9 @@ for p in products:
 	print(p[0]) #小清單第0格
 	print(p[0], '的價格是:', p[1])
 
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n' ) #','做不同型別的區隔, '\n'換行
 
 #	products[0][0]
 
